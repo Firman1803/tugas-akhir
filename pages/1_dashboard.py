53be5a04d5d615dc6c26f44d0368061f1583d021
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 09:50:26 +0700

    Create 1_dashboard.py

---
+++ pages/1_dashboard.py
@@ -0,0 +1,24 @@
+# pages/1_Dashboard.py
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Dashboard - Exploratory Data Analysis")
+
+# Load dataset
+@st.cache_data
+def load_data():
+    return pd.read_csv("predic_tabel.csv")
+
+df = load_data()
+st.write("### Data Sampel")
+st.dataframe(df.head())
+
+st.write("### Distribusi Usia")
+sns.countplot(x="Usia", data=df)
+st.pyplot(plt)
+
+st.write("### Distribusi Hasil")
+sns.countplot(x="Hasil", data=df)
+st.pyplot(plt)
